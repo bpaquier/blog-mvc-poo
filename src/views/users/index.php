0890e8349856be88129e50fd6bda729bb1e0bfbf
quentingrchr <51860690+quentingrchr@users.noreply.github.com>
2021-12-01 12:13:34 +0100

    add others layout

---
+++ src/views/users/index.php
@@ -0,0 +1,38 @@
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Email</th>
+      <th scope="col">Is Admin</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Jean François</td>
+      <td>DI RIENZO</td>
+      <td>@jfdiri@gmail.com</td>
+      <td>
+        <input type="checkbox" name="is-admin" id="admin" checked>
+      </td>
+      <td>
+        <a href="#" class="btn btn-danger">Delete</a>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Francis</td>
+      <td>Huster</td>
+      <td>francis@gmail.com</td>
+      <td>
+        <input type="checkbox" name="is-admin" id="admin">
+      </td>
+      <td>
+        <a href="#" class="btn btn-danger">Delete</a>
+      </td>
+    </tr>
+  </tbody>
+</table>
