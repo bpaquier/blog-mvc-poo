2b09bd47861beb5bf41d38bea78737f0d7086492
bpaquier <bastien.paquier@gmail.com>
2021-12-01 18:33:37 +0100

    feat(posts) dynamise template

--- src/Model/PostManager.php
+++ src/Model/PostManager.php
@@ -4,5 +4,20 @@ namespace App\Model;
 
 class PostManager extends BaseManager
 {
+    public function getAllPosts() {
+        $query = $this->db->prepare('SELECT * FROM posts');
+        $query->execute();
+        $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Entity\Posts');
 
+        return $query->fetchAll();
+    }
+
+    public function getPostById(int $id) {
+        $query = $this->db->prepare('SELECT * FROM posts where id = :id');
+        $query->bindValue(':id', $id, \PDO::PARAM_INT);
+        $query->execute();
+        $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Entity\Posts');
+
+        return $query->fetch();
+    }
 }
--- src/View/posts/index.php
+++ src/View/posts/index.php
@@ -1,34 +1,27 @@
-<div class="posts-list">
-  <div class="card-deck">
-    <div class="card">
-      <img class="card-img-top" src="https://source.unsplash.com/1600x900/?beach" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Author: User 1</small></p>
-        <p class="card-text"><small class="text-muted">Published 3 mins ago</small></p>
-        <a href="#" class="btn btn-primary">Read more</a>
-      </div>
-    </div>
-    <div class="card">
-      <img class="card-img-top" src="https://source.unsplash.com/1600x900/?beach" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Author: User 1</small></p>
-        <p class="card-text"><small class="text-muted">Published 3 mins ago</small></p>
-        <a href="#" class="btn btn-primary">Read more</a>
-      </div>
-    </div>
-    <div class="card">
-      <img class="card-img-top" src="https://source.unsplash.com/1600x900/?beach" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Title</h5>
-        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Author: User 1</small></p>
-        <p class="card-text"><small class="text-muted">Published 3 mins ago</small></p>
-        <a href="#" class="btn btn-primary">Read More</a>
-      </div>
-    </div>
-  </div>
-</div>
+<?php
+    $posts = $data;
+
+    if($posts){
+        ?>
+            <div class="posts-list">
+                <div class="card-deck">
+                    <?php foreach ($posts as $post): ?>
+                        <div class="card">
+                            <img class="card-img-top" src="https://source.unsplash.com/1600x900/?beach" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $post['title'] ?></h5>
+                                <p class="card-text"><?= substr($post['content'],0,150) . "..." ?></p>
+                                <p class="card-text"><small class="text-muted"><?= $post['author'] ?></small></p>
+                                <p class="card-text"><small class="text-muted"><?= $post['date'] ?></small></p>
+                                <a href="?p=post&id=<?= $post['id'] ?>" class="btn btn-primary">See Post</a>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php
+    } else {
+        echo "<p>Zero Posts</p>";
+    }
+?>
+
